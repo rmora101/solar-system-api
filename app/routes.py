66235011777de4134f0c40ad7ee7b68996249f85
Rediet Medhane <redietkmedhane@gmail.com>
2023-04-20 11:30:34 -0700

    created Planet class

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+class Planet:
+    def __init__(self, id, name, description, num_of_moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.num_of_moons = num_of_moons
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods= ["GET"])
 
