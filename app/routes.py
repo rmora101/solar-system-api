9cf8ecc2382533307bc3c5f70afa33b02ea93ac8
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-04-20 11:51:09 -0700

    added a routes file

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,32 @@ class Planet:
         self.description = description
         self.num_of_moons = num_of_moons
 
+planet1 = Planet(1, "Mercury", "The atmosphere of Mercury is a surface bounded exosphere, essentially a vacuum.", 0)
+planet2 = Planet(2, "Venus", "Venus is the hottest planet.", 0)
+planet3 = Planet(3, "Earth", "Earth is our home.", 1)
+planet4 = Planet(4, "Mars", "Mars is red", 2)
+planet5 = Planet(5, "Jupiter", "Jupiter is the biggest", 92)
+planet6 = Planet(6, "Saturn", "Saturn has a big ring", 83)
+planet7 = Planet(7, "Uranus", "Uranus has a unique tilt", 27)
+planet8 = Planet(8, "Neptune", "Neptune is the only planet not visible to the naked eye", 14)
+planet9 = Planet(9, "Pluto", "Pluto is a PLANET", 5)
+
+planet_list = [planet1, planet2, planet3, planet4, planet5, planet6, planet7, planet8, planet9]
+
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods= ["GET"])
+def get_planets():
+    response = []
+    for planet in planet_list:
+        planet_dict = {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "number of moons": planet.num_of_moons
 
+        }
+
+        response.append(planet_dict)
+
+    return jsonify(response), 200
