d908d37225d06556ce7f7084d66e0a8e59ffe674
Rediet Medhane <redietkmedhane@gmail.com>
2023-04-27 13:26:41 -0700

    sets up Planet model

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    db  = SQLAlchemy()
+    migrate = Migrate()
+
+    app.config["SQLALCHEMY_TRACK_NOTIFICATIONS"] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development
+
+    db.init_app(app)
+    migrate.init_app(app)
+
+    from app.models.planet import Planet
+    
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
 
