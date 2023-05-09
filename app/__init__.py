c6d9120b23626c7f8a40c21c6139bfb719c83a09
rmora101 <59456883+rmora101@users.noreply.github.com>
2023-05-09 11:45:18 -0700

    added uri

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ def create_app(test_config=None):
 
     if not test_config:
 
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("RENDER_DATABASE_URI")
     else:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
 
